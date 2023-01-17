908c51b918c8f090cae5a8d9e072d5edf0a4a41f
Naturealizor <31334963+Naturealizor@users.noreply.github.com>
2023-01-17 00:40:55 -0500

    added flyout

---
+++ src/components/FlyTo.svelte
@@ -0,0 +1,10 @@
+<script>
+  function flyTo() {
+    map.flyTo({
+      center: [-75.49375, 39.05163],
+      zoom: 24,
+    });
+  }
+</script>
+
+<button on:click={flyTo} id="flyTo">Recenter</button>
